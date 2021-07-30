64d7b15fbde1e88a65362f0234789a320421faed
Hridaya Agrawal <hridayahoney@gmail.com>
2021-07-30 15:31:53 +0530

    Update Education.js

--- src/containers/education/Education.js
+++ src/containers/education/Education.js
@@ -1,18 +1,27 @@
-import React from "react";
 import "./Education.scss";
+import "./WorkExperience.scss";
 import EducationCard from "../../components/educationCard/EducationCard";
 import {educationInfo} from "../../portfolio";
-
+import React, {useContext} from "react";
+import {Fade} from "react-reveal";
+import StyleContext from "../../contexts/StyleContext";
 export default function Education() {
   if (educationInfo.display) {
     return (
-      <div className="education-section" id="education">
-        <h1 className="education-heading">Education</h1>
-        <div className="education-card-container">
-          {educationInfo.schools.map((school, index) => (
-            <EducationCard key={index} school={school} />
-          ))}
-        </div>
+      <div id="experience">
+        <Fade bottom duration={1000} distance="20px">
+          <div className="experience-container" id="workExperience">
+            <div>
+              <h1 className="experience-heading">Experiences</h1>
+              <div className="experience-cards-div">
+                {educationInfo.schools.map((school, i) => {
+                  return (
+                    <EducationCard key={i} school={school} /> );
+               })}
+              </div>
+            </div>
+          </div>
+        </Fade>
       </div>
     );
   }
