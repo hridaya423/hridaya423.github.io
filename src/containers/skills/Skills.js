8d2c6c14182676c9bb74386b35236c6538ba2f76
Hridaya Agrawal <hridayahoney@gmail.com>
2021-08-03 15:01:19 +0530

    Update Skills.js

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -61,7 +61,7 @@ export default function Skills() {
                     >
                       {bullet}
                     </p> )
-                }
+                },
                </div>
                 );
               })}
