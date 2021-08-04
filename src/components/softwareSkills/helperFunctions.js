5d120eb5ae5b71e3e94e816dc5302c1319a393b5
Hridaya Agrawal <hridayahoney@gmail.com>
2021-08-04 20:15:54 +0530

    Update helperFunctions.js

--- src/components/softwareSkills/helperFunctions.js
+++ src/components/softwareSkills/helperFunctions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const getDetails = (skill, name) => {
-  if name == "Tensorflow" || name == "Keras" || name == "PyTorch" {
+  if (name == "Tensorflow" || name == "Keras" || name == "PyTorch") {
     return <img src={skill.image} alt={name} ></img>
   } else {
     return <i className={skills.fontAwesomeClassname} style={{ color: skills.color }}></i>
